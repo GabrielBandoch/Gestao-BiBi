7ba0c9c18b8cbdf57721564c7f3b5b9dcd040679
JGTamanini <jgtd.dalmarco@outlook.com>
2025-06-19 18:59:40 -0300

    setup telas do front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import PageCadastro from './Pages/PageCadastro';
+import PageLogin from './Pages/PageLogin';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/cadastro" element={<PageCadastro />} />
+        <Route path="/login" element={<PageLogin />} />
       </Routes>
     </BrowserRouter>
   )
