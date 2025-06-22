8ad25e9f0f8daed2d5306ed22432938019330856
Felipe <felipe.chawischi@catolicasc.edu.br>
2025-06-22 17:14:58 -0300

    att

--- backend/api/models/User.js
+++ backend/api/models/User.js
@@ -1,12 +1,26 @@
 module.exports = {
-    datastore: 'postgreSQL',
-    attributes: {
-      id: {
+  tableName: 'Usuario', // botar esaa merda, ver depois
+  primaryKey: 'id',     //
+  datastore: 'postgreSQL',
+  attributes: {
+    id: {
       type: 'number',
       columnType: 'serial',
       autoIncrement: true,
     },
-      nome: { type: 'string' },
-      email: { type: 'string' }
+    nome: { type: 'string' },
+    email: { type: 'string' },
+    
+    createdAt: {
+      type: 'ref', 
+      columnType: 'timestamp without time zone',
+      autoCreatedAt: true 
     },
-  };
+    updatedAt: {
+      type: 'ref', 
+      columnType: 'timestamp without time zone',
+      autoUpdatedAt: true 
+    },
+  },
+};
+
